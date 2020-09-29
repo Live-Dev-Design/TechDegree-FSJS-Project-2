7812a58ef90f6d59b78f17ce781a1f97da11b126
Ryan Brecher <52445175+Live-Dev-Design@users.noreply.github.com>
2020-09-29 12:14:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TechDegree-FSJS-Project-2
+ Data Pagination and Filtering
---
+++ js/script.js
@@ -0,0 +1,20 @@
+/***********************************************
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+***********************************************/
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+
+
+// Call functions
